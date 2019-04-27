a289662050c474521a0cb1e455ae1e72b7e065d4
Slanpik <fjsanchezval@gmail.com>
2019-04-27 15:01:44 -0500

    ▲ Primer Commit ▲

---
+++ database/migrations/2019_04_27_195007_create_users_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name', 100);
+            $table->string('last_name', 100)->nullable();
+            $table->string('email', 300);
+            $table->timestamps();
+        });
+    }
+
+    /**
